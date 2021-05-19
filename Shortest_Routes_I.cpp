4ddfa0603677146573be77935cbcaae6c0276810
Ayush kumar <ayushkr667@gmail.com>
2021-05-19 14:27:18 +0530

    Update Shortest_Routes_I.cpp

--- Shortest_Routes_I.cpp
+++ Shortest_Routes_I.cpp
@@ -1,11 +1,3 @@
-/*-------------------------------*
-| Name: Ayush Kumar              |
-| College: SOE, CUSAT            |
-| Dept: CSE                      |
-| Site: CSES                     |
-| Email: ayushkr667@gmail.com    |
-*-------------------------------*/
- 
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
